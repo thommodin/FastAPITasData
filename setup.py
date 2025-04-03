f0cd30ba5b72fdca64cca8829964ca8dcc0bbf2f
thommodin <thommygalindo@gmail.com>
2025-04-03 14:58:43 +1100

    Added setuptools toml w running entrypoint

--- setup.py
+++ setup.py
@@ -1,9 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='FastAPITasData',
-    version='0.1.0',
-    packages=find_packages(
-        where='src'
-    ),
-)
+from setuptools import setup
+setup()
